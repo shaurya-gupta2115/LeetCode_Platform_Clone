f07c6b3215650d4969810e57a42211b8214b2fe1
Shaurya Gupta <vishnu8858369983@gmail.com>
2025-05-01 19:53:57 +0530

    Server running success

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-//index.js
-const express = require("express")
+const express = require("express");
 const app = express();
 require("dotenv").config();
 
+app.get("/hello", (req, res) => {
+  res.send("hello World baby ");
+});
 
-app.get("/",(req,res)=>{
-    res.send("Hello Project")
-})
-const port = process.env.PORT;
-
-app.listen(port, () => {
-    console.log("Server started running :) at localhost")
-})
-
+app.listen(process.env.PORT, () => {
+  console.log("Server started running :) at :" + process.env.PORT);
+});
